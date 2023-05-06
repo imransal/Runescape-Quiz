8644fbbe22463c609cd90ac169f17a3b3417de31
imransal <>
2023-05-06 09:37:26 +0000

    changed page location to leader baord isntead of home page  after clicking save on end page

--- assets/js/end.js
+++ assets/js/end.js
@@ -31,5 +31,5 @@ let saveHighscore = (e) => {
     highScores.splice(5);
 
     localStorage.setItem(`highScores`, JSON.stringify(highScores));
-    window.location.assign(`/`);
+    window.location.assign(`leaderboard.html`);
 };
